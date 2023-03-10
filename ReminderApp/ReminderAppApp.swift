260932135c71cc2551f1f1faa8dc2b1aa25d7557
Mertcncifter <Mertcncifter@gmail.com>
2023-03-10 22:58:36 +0300

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by mert can çifter on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
